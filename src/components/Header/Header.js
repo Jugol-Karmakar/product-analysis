1fbe626abb31718ec88adf07a6f1783e1053e907
JugolKarmakar <jugol.cse@gmail.com>
2022-04-04 19:11:39 +0600

    customLink commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Customlink from "../Customlink/Customlink";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <nav className="header">
-      <Link to="/home">Home</Link>
-      <Link to="/review">Review</Link>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/about">About</Link>
+    <nav className="navbar">
+      <Customlink to="/home">Home</Customlink>
+      <Customlink to="/review">Review</Customlink>
+      <Customlink to="/dashboard">Dashboard</Customlink>
+      <Customlink to="/blog">Blog</Customlink>
+      <Customlink to="/about">About</Customlink>
     </nav>
   );
 };
