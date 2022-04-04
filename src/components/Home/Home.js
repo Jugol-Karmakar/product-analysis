c2f97211e6200985d8387f223198ced12153c85d
JugolKarmakar <jugol.cse@gmail.com>
2022-04-04 21:17:45 +0600

    review commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,12 @@
 import React from "react";
-import "./Home.css";
+import Hero from "../Hero/Hero";
+import Review from "../Review/Review";
 
 const Home = () => {
   return (
-    <div className="header-container">
-      <div className="laptop-title">
-        <h1>Your Next Laptop</h1>
-        <h1 className="blue-color">Your Best Laptop</h1>
-        <p>
-          Laptops combine all the input/output components and capabilities of a
-          desktop computer, including the display screen, small speakers, a
-          keyboard, data storage device, sometimes an optical disc drive,
-          pointing devices (such as a touch pad or pointing stick), with an
-          operating system, a processor and memory into a single unit.
-        </p>
-        <button className="laptop-btn">Learn More</button>
-      </div>
-      <div className="laptop-image">
-        <img
-          src="https://img.freepik.com/free-psd/laptop-mock-up-isolated_1310-1482.jpg?t=st=1649060760~exp=1649061360~hmac=d8e9184bb1998a5de51de9a14c228cdfba51bd74076db784d40512a975d42d36&w=740"
-          alt=""
-        />
-      </div>
+    <div>
+      <Hero></Hero>
+      <Review></Review>
     </div>
   );
 };
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CustomerReview from "../CustomerReview/CustomerReview";
 
 const Review = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("review.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
   return (
-    <div>
-      <h2>hello review</h2>
+    <div className="bg-orange-200">
+      <h2 className="text-center text-3xl font-semibold py-3">
+        Customer Review
+      </h2>
+      <div className="grid md:grid-cols-3 gap-6 mx-20 py-10 ">
+        {reviews.map((review) => (
+          <CustomerReview key={review.id} review={review}></CustomerReview>
+        ))}
+      </div>
     </div>
   );
 };
